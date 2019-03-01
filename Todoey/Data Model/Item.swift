802236a276f9906efd287e7205d2dbdb2ba77883
Celil Pembeoğlu <celilpembeoglu@gmail.com>
2019-03-01 18:38:48 +0300

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by celil pembeoğlu on 1.03.2019.
+//  Copyright © 2019 Celil Pembeoğlu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
